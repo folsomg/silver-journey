0be67f52d0756f65e5f0a205684d1ef798e3835f
folsomg <folsomg@users.noreply.github.com>
2025-10-13 11:18:06 +0300

    Correct date formatting

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@
 export function formatDate(date) { // updated
     return date.toISOString().split('T')[0];
 }
-
+ // updated
 export function validateEmail(email) {
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
